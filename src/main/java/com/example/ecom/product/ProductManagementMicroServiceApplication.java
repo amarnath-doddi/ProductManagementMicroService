c98c713881e4981800dfab676f728d6ccbd3a9d5
amarnath-doddi <31587643+amarnath-doddi@users.noreply.github.com>
2021-06-12 03:17:22 +0530

    Add files via upload

---
+++ src/main/java/com/example/ecom/product/ProductManagementMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecom.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagementMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagementMicroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ecom/product/ProductManagementMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecom.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagementMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
